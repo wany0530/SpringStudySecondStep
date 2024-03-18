f21f52e7ce22ac0e287970f2330aa2dfe12b96dc
LeeJaeWan <ljw6398@hanmail.net>
2024-03-18 23:12:54 +0900

    commit

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
@@ -12,11 +13,23 @@ public class AppConfig
 {
     public MemberService memberService()
     {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService()
     {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
+    private MemoryMemberRepository memberRepository()
+    {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy()
+    {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+
 }
